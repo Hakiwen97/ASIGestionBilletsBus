63552eb600cbcc80c554a1b7594a3816c9458072
Hakiwen97 <adriensigrist@hotmail.com>
2021-06-02 13:53:58 +0200

    first commit

---
+++ src/main/java/com/example/gestionbilletsbus/GestionbilletsbusApplication.java
@@ -0,0 +1,13 @@
+package com.example.gestionbilletsbus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionbilletsbusApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GestionbilletsbusApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/gestionbilletsbus/GestionbilletsbusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gestionbilletsbus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionbilletsbusApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
