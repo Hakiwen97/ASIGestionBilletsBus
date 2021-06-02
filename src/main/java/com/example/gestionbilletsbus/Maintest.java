706b18a6199e30e1dba535b3329691a4635f2fe2
Hakiwen97 <adriensigrist@hotmail.com>
2021-06-02 14:02:37 +0200

    Ajout du code pour créer un code qr

--- src/main/java/com/example/gestionbilletsbus/Maintest.java
+++ src/main/java/com/example/gestionbilletsbus/Maintest.java
@@ -1,9 +1,39 @@
 package com.example.gestionbilletsbus;
 
+import static com.example.service.QrCodeGenerator.createQR;
+
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.WriterException;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Maintest {
 
-  public static void main(String[] args) {
-    System.out.println("hello");
+  public static void main(String[] args) throws IOException, WriterException {
+
+    // The data that the QR code will contain
+    String data = "www.geeksforgeeks.org";
+
+    // The path where the image will get saved
+    String path = "C:\\ASI\\ASIGestionBilletsBus\\images\\test.png";
+
+    // Encoding charset
+    String charset = "UTF-8";
+
+    Map<EncodeHintType, ErrorCorrectionLevel> hashMap
+        = new HashMap<EncodeHintType,
+        ErrorCorrectionLevel>();
+
+    hashMap.put(EncodeHintType.ERROR_CORRECTION,
+        ErrorCorrectionLevel.L);
+
+    // Create the QR code and save
+    // in the specified folder
+    // as a jpg file
+    createQR(data, path, charset, hashMap, 200, 200);
+    System.out.println("QR Code Generated!!! ");
   }
 
 }
--- src/main/java/com/example/service/QrCodeGenerator.java
+++ src/main/java/com/example/service/QrCodeGenerator.java
@@ -1,6 +1,36 @@
 package com.example.service;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.NotFoundException;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
+
 public class QrCodeGenerator {
+
+  // Function to create the QR code
+  public static void createQR(String data, String path,
+      String charset, Map hashMap,
+      int height, int width)
+      throws WriterException, IOException
+  {
+
+    BitMatrix matrix = new MultiFormatWriter().encode(
+        new String(data.getBytes(charset), charset),
+        BarcodeFormat.QR_CODE, width, height);
+
+    MatrixToImageWriter.writeToFile(
+        matrix,
+        path.substring(path.lastIndexOf('.') + 1),
+        new File(path));
+  }
 
 
 }
